32595541e6bee9ffa1c703ec53261d6270fa17d5
Ricardo da Silva Ogliari <rogliariping@gmail.com>
2023-10-08 20:57:47 -0300

    Step 15

--- lib/task_cell.dart
+++ lib/task_cell.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:to_do_list/model/task.dart';
+import 'package:to_do_list/presentation/task_cubit.dart';
 
 class TaskCell extends StatelessWidget {
   final Task task;
@@ -11,6 +13,18 @@ class TaskCell extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(task.text);
+    return Row(
+      children: [
+        Expanded(
+          child: Text(task.text),
+        ),
+        IconButton(
+          onPressed: () => context.read<TaskCubit>().completeOneTask(task),
+          icon: Icon(
+            task.isDone ? Icons.check : Icons.circle,
+          ),
+        )
+      ],
+    );
   }
 }
