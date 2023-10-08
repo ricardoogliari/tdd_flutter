6b5e1c724eca02a79530b9238f3aff49ff19fcab
Ricardo da Silva Ogliari <rogliariping@gmail.com>
2023-10-08 10:32:50 -0300

    Step 3

--- lib/presentation/task_cubit.dart
+++ lib/presentation/task_cubit.dart
@@ -7,5 +7,11 @@ part 'task_state.dart';
 class TaskCubit extends Cubit<TaskState> {
   TaskCubit() : super(TaskInitial());
 
-  addOneTask(Task task) => emit(TaskUpdated(tasks: <Task>[task]));
+  addOneTask(Task task) {
+    if (state is TaskInitial) {
+      emit(TaskUpdated(tasks: [task]));
+    } else if (state is TaskUpdated) {
+      emit(TaskUpdated(tasks: [...(state as TaskUpdated).tasks, task]));
+    }
+  }
 }
